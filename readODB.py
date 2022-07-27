db2d27902e020718c85b75a1e0f43bd0e3a4a0c7
anqiclaire <40797584+anqiclaire@users.noreply.github.com>
2022-07-27 11:13:06 -0400

    plane stress setup

--- readODB.py
+++ readODB.py
@@ -4,16 +4,10 @@ import csv
 
 from odbAccess import openOdb
 from abaqusConstants import NODAL
-from poisson_eff import get_poisson
 
 # abaqus trims sys.argv until after "python"
-if len(sys.argv) < 4:
-	this_script, name, displacement = sys.argv
-	poisson = get_poisson(name)
-else:
-	this_script, name, displacement, poisson = sys.argv
+this_script, name, displacement = sys.argv
 displacement = float(displacement)
-poisson = float(poisson)
 tsv = csv.writer(open(name+'-youngs.tsv', 'wb'), dialect=csv.excel_tab)
 tsv.writerow(('frequency (Hz)', 'E_Real (Pa)', 'E_Imag (Pa)'))
 odb = openOdb(name+'.odb', readOnly=True)
@@ -26,6 +20,6 @@ for frame in odb.steps['STEP-1'].frames:
 	E_Real = 0
 	E_Imag = 0
 	for v in RF.values:
-		E_Real += v.data[0] / displacement * (1 - poisson**2)
-		E_Imag += v.conjugateData[0] / displacement * (1 - poisson**2)
+		E_Real += v.data[0] / displacement
+		E_Imag += v.conjugateData[0] / displacement
 	tsv.writerow((frequency, E_Real, E_Imag))
